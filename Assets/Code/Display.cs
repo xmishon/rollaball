322dcbdf3f55e66e35e27ee8189bf274cf137d40
Mikhail Zmeevskiy <xmishon17@gmail.com>
2020-11-03 03:56:31 +0300

    Added pickup bonus events

---
+++ Assets/Code/Display.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Debug;
+
+public sealed class Display
+{
+    private Text _bonusesCountTextField;
+    private Text _gameFinishedTextField;
+
+    public Display()
+    {
+        _bonusesCountTextField = GameObject.Find("BonusesCountText").GetComponent<Text>();
+        _gameFinishedTextField = GameObject.Find("GameFinishedText").GetComponent<Text>();
+    }
+
+    public void DisplayBonuses(int value)
+    {
+        _bonusesCountTextField.text = $"Вы набрали {value}";
+        Log("Изменено количество бонусов: " + value);
+    }
+
+    public void DisplayGameFinished()
+    {
+        _bonusesCountTextField.text = "";
+        _gameFinishedTextField.text = "Победа";
+        Log("Игра закончена");
+    }
+}
---
+++ Assets/Code/SoundController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace mzmeevskiy
+{
+    public class SoundController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+
+        public void PlayBonusPickupSound()
+        {
+            _audioSource.Play();
+        }
+    }
+}
